3f3e1cd247e1c28c36ebde6a6caf44bbb15b66f1
Luca Orofino <lucaoro455@gmail.com>
2024-10-17 11:07:40 +0200

    some fixes

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# eth-advanced
+# ETH-ADV Project - Smart Contract
+
+## 1. Overview
+
+Il contratto **nftProject** è uno smart contract basato su Ethereum che permette la creazione, visualizzazione e scambio di NFT utilizzando Chainlink VRF per garantire l'assegnazione casuale e sicura degli ID degli NFT. 
+Il contratto gestisce una collezione limitata di NFT, ognuno con un set di metadati associati (nome della rivista, autore, descrizione e nome della collezione).
+
+## 2. Purpose
+
+Questo progetto ha come obiettivo di fornire alle aziende uno strumento affidabile per creare e vendere NFT unici che garantiscano autenticità e tracciabilità dei loro prodotti. Per un'azienda nel settore della divulgazione scientifica, la creazione di NFT potrebbe rappresentare un'opportunità di monetizzazione, di coinvolgimento e di espansione del pubblico attraverso collezionabili digitali unici. 
+
+
+### Vantaggi principali:
+- **Autenticità garantita**: Gli NFT creati tramite questo contratto sono legati a un ID univoco generato in modo trasparente e casuale (garantito da Chainlink VFR).
+- **Tracciabilità**: Ogni transazione relativa agli NFT è registrata su blockchain, rendendo la provenienza e il passaggio di proprietà completamente tracciabili.
+- **Monetizzazione**: Le aziende possono vendere NFT e ricevere direttamente pagamenti in Ether tramite le fee di transazione.
+
+## 3. Technical Choice
+
+Il contratto utilizza **OpenZeppelin** per l'implementazione dello standard ERC721 (NFT) e **Chainlink VRF** per la generazione di numeri casuali sicuri. 
+- **ERC721**: Il contratto implementa l'interfaccia standard per NFT, rendendo i token compatibili con vari marketplace e wallet.
+- **Chainlink VRF**: Utilizzato per richiedere numeri casuali in modo verificabile, al fine di assegnare ID unici agli NFT creati.
+- **Exchange**: Gli utenti possono scambiare NFT.
+
+
+## Development Environment
+- **Solidity Version**: ^0.8.20.
+- **Events for Transparency**: Le principali azioni del contratto sono tracciate attraverso eventi.
+  
+## Deployment
+- **Deployment Order**: Il contratto segue una sequenza di deploy. È importante che i parametri come subscriptionId e keyHash di Chainlink VRF siano corretti prima del deploy, in modo che il contratto funzioni correttamente e le comunicazioni tra i moduli siano gestite correttamente.
+
+
--- test/nft-test.js
+++ test/nft-test.js
@@ -4,9 +4,9 @@ const { ethers } = require("hardhat");
 
 describe("nftProject", function () {
     let NftProject, nftProject, owner, addr1, addr2;
-    const collectionName = "Test Collection";
-    const maxSupply = 10;
-    const mintCost = ethers.utils.parseEther("0.01"); // 0.01 ether
+    const collectionName = "Luca Collection";
+    const maxSupply = 4;
+    const mintCost = ethers.utils.parseGwei("1");
 
     beforeEach(async function () {
         NftProject = await ethers.getContractFactory("nftProject");
@@ -20,72 +20,70 @@ describe("nftProject", function () {
             expect(await nftProject.owner()).to.equal(owner.address);
         });
 
-        it("Should set the collection name correctly", async function () {
+        it("Should set the collection name", async function () {
             expect(await nftProject.collectionName()).to.equal(collectionName);
         });
 
-        it("Should set the maximum supply correctly", async function () {
+        it("Should set the maximum supply", async function () {
             expect(await nftProject.maxSupply()).to.equal(maxSupply);
         });
     });
 
-    describe("Minting NFTs", function () {
-        it("Should mint an NFT correctly", async function () {
+    describe("Create NFTs", function () {
+        it("Should create an NFT correctly", async function () {
             await addr1.sendTransaction({ to: nftProject.address, value: mintCost });
-            await nftProject.connect(addr1).createNFT("Magazine A", "Author A", "Description A");
+            await nftProject.connect(addr1).createNFT("Luca Collection", "Luca", "This's my collection");
 
-            const nftId = 1; // The first minted NFT will have ID 1
-            const metadata = await nftProject.viewNFT(nftId);
 
-            expect(metadata[0]).to.equal("Magazine A");
-            expect(metadata[1]).to.equal("Author A");
-            expect(metadata[2]).to.equal("Description A");
+            expect(metadata[0]).to.equal("Luca Collection");
+            expect(metadata[1]).to.equal("Luca");
+            expect(metadata[2]).to.equal("This's my collection");
             expect(metadata[3]).to.equal(collectionName);
             expect(metadata[4]).to.equal(addr1.address);
         });
 
-        it("Should not allow minting more than max supply", async function () {
+        it("Should not allow to create more nft than max supply settled", async function () {
             for (let i = 0; i < maxSupply; i++) {
                 await addr1.sendTransaction({ to: nftProject.address, value: mintCost });
                 await nftProject.connect(addr1).createNFT(`Magazine ${i}`, `Author ${i}`, `Description ${i}`);
             }
 
             await addr1.sendTransaction({ to: nftProject.address, value: mintCost });
-            await expect(nftProject.connect(addr1).createNFT("Magazine Overflow", "Author Overflow", "Description Overflow"))
+            await expect(nftProject.connect(addr1).createNFT("Magazine", "Author", "Description"))
                 .to.be.revertedWith("Max supply reached");
         });
 
-        it("Should not allow minting without enough ether", async function () {
-            await expect(nftProject.connect(addr1).createNFT("Magazine A", "Author A", "Description A"))
-                .to.be.revertedWith("Not enough ether to create NFT");
+        it("Should not allow create without enough gwei", async function () {
+            await expect(nftProject.connect(addr1).createNFT("Luca Collection", "Luca", "This's my collection"))
+                .to.be.revertedWith("Not enough gwei");
         });
     });
 
     describe("Transferring NFTs", function () {
-        it("Should transfer an NFT correctly", async function () {
+        it("Should transfer an NFT", async function () {
             await addr1.sendTransaction({ to: nftProject.address, value: mintCost });
-            await nftProject.connect(addr1).createNFT("Magazine A", "Author A", "Description A");
+            await nftProject.connect(addr1).createNFT("Luca Collection", "Luca", "This's my collection");
 
-            const nftId = 1; // The first minted NFT will have ID 1
+            const nftId = 1;
             await nftProject.connect(addr1).exchangeNFT(addr2.address, nftId);
 
             expect(await nftProject.ownerOf(nftId)).to.equal(addr2.address);
         });
 
-        it("Should not allow transferring an NFT by a non-owner", async function () {
+        it("Should allow to transfer nft only to their owner", async function () {
             await addr1.sendTransaction({ to: nftProject.address, value: mintCost });
-            await nftProject.connect(addr1).createNFT("Magazine A", "Author A", "Description A");
+            await nftProject.connect(addr1).createNFT("Luca Collection", "Luca", "This's my collection");
 
-            const nftId = 1; // The first minted NFT will have ID 1
+            const nftId = 1;
             await expect(nftProject.connect(addr2).exchangeNFT(addr1.address, nftId))
                 .to.be.revertedWith("You are not the owner of this NFT");
         });
 
-        it("Should not allow transferring to the zero address", async function () {
+        it("Should not allow transferring to invalid address", async function () {
             await addr1.sendTransaction({ to: nftProject.address, value: mintCost });
-            await nftProject.connect(addr1).createNFT("Magazine A", "Author A", "Description A");
+            await nftProject.connect(addr1).createNFT("Luca Collection", "Luca", "This's my collection");
 
-            const nftId = 1; // The first minted NFT will have ID 1
+            const nftId = 1;
             await expect(nftProject.connect(addr1).exchangeNFT(ethers.constants.AddressZero, nftId))
                 .to.be.revertedWith("Invalid address");
         });
@@ -94,12 +92,12 @@ describe("nftProject", function () {
     describe("Withdrawals", function () {
         it("Should not allow to withdraw except for the owener", async function () {
             await expect(nftProject.connect(addr1).withdraw())
-                .to.be.revertedWith("Not the contract owner");
+                .to.be.revertedWith("Your not the owner");
         });
 
         it("Should not allow withdrawing with no funds", async function () {
             await expect(nftProject.withdraw())
-                .to.be.revertedWith("No funds to withdraw");
+                .to.be.revertedWith("No funds available to withdraw");
         });
     });
 });
